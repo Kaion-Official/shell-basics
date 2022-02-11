3bdd3cf41fadc5d37df401bb2c1da168d2981d72
Kaion-Official <86653534+Kaion-Official@users.noreply.github.com>
2022-02-11 10:08:27 +0000

    What is your name script

---
+++ Hello/main.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#A simple script that asks for your name then outputs it.
+
+echo -n "Hello! What's your name? "
+read name
+echo "Hello$name!"
+echo "Let's go do some sweet shell coding!"
